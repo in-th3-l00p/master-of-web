41eda9f7b16852da44dc4bf9d677340dc98331e0
in-th3-l00p <indianucode0303@gmail.com>
2024-12-02 23:35:56 +0200

    fixed login form

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -59,7 +59,7 @@ const LoginForm: React.FC = () => {
     };
 
     return (
-        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="flex min-h-full flex-col justify-center px-6 pt-36 pb-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img className="mx-auto h-10 w-auto" src="/logo.svg" alt="Master of Web" />
                 <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
